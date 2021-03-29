5c4f1b1a53c063b476d677b2b1723dfaf7ba090f
Oliver Smit <48744491+Osbbyx@users.noreply.github.com>
2021-03-29 12:05:11 -0400

    Parralax(EASY)- con materiales

---
+++ Assets/lol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lol : MonoBehaviour
+{
+    public float speed;
+    private Renderer rnd;
+
+    void Start()
+    {
+        rnd = GetComponent<Renderer>();
+    }
+
+
+    void Update()
+    {
+        rnd.material.mainTextureOffset = new Vector2(rnd.material.mainTextureOffset.x + speed * Time.deltaTime, 0);
+    }
+}
